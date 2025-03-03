3e3e3d72115602bd90ed1929e1764d162d01925e
Cestercian <yashafaid@gmail.com>
2025-03-04 02:09:26 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -10,29 +10,27 @@ type Order = {
     status: "ordered"|"unordered"|"completed",
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
-]
-
 let cashInRegister = 100
 let nextOrderId = 1
+let nextPizzaId = 1
+
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: "Margherita", price: 8 },
+    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaId++, name: "Veggie", price: 9 },
+]
+
 const orderQueue: Order[] = [];
 
-function addNewPizza(pizzaObj: Pizza): void {
-    menu.push(pizzaObj)
+function addNewPizza(pizzaObj: Omit<Pizza,"id">): void {
+    const pizza = {id: nextPizzaId++,...pizzaObj,}
+    menu.push(pizza)
 }
-
-/**
- * Challenge: add explicit return types to the rest of our functions
- */
 
 function placeOrder(pizzaName: string) : Order {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
     if (!selectedPizza) {
-        //console.error(`${pizzaName} does not exist in the menu`)
         throw new TypeError(`${pizzaName} is not in the menu`)
     }
     cashInRegister += selectedPizza.price
@@ -64,9 +62,9 @@ export function getPizzaDetail(identifier: string | number): Pizza | undefined {
     }
 }
 
-addNewPizza({ id: 4, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 5, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 6, name: "Spicy Sausage", price: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
